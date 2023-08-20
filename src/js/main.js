5e62b0d4589f233615852dbf65ab342a1623d306
AlenaGM <alena.guillaume4@gmail.com>
2023-08-20 15:47:47 +0200

    feature: added swiper and styled story section

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,13 @@
-window.addEventListener('DOMContentLoaded', () => {
-    console.log('start')
-})
-
+window.addEventListener("DOMContentLoaded", () => {
+  const swiper = new Swiper(".swiper", {
+    loop: true,
+    centeredSlides: true,
+    slidesPerView: 1.4,
+    spaceBetween: 30,
+    initialSlide: 1,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+  });
+});
