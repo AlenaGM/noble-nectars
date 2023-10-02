fe178f9df1d10f9e2aab0e6dc62d8405315d3735
AlenaGM <alena.guillaume4@gmail.com>
2023-10-02 14:45:37 +0200

    minor fixes save before anim adaptation

--- src/js/main.js
+++ src/js/main.js
@@ -2,27 +2,26 @@
 window.addEventListener("DOMContentLoaded", () => {
   anim();
 
-  // eslint-disable-next-line no-undef
-  //const swiper = new Swiper(".swiper", {
-  //  loop: true,
-  //  autoplay: {
-  //    delay: 2000,
-  //    disableOnInteraction: false,
-  //  },
-  //  breakpoints: {
-  //    320: {
-  //      slidesPerView: 1.1,
-  //      spaceBetween: 20,
-  //      centeredSlides: false,
-  //    },
-  //    450: {
-  //      slidesPerView: 1.4,
-  //      spaceBetween: 30,
-  //      centeredSlides: true,
-  //      initialSlide: 1,
-  //    },
-  //  },
-  //});
+  const swiper = new Swiper(".swiper", {
+    loop: true,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1.1,
+        spaceBetween: 20,
+        centeredSlides: false,
+      },
+      450: {
+        slidesPerView: 1.4,
+        spaceBetween: 30,
+        centeredSlides: true,
+        initialSlide: 1,
+      },
+    },
+  });
 });
 
 function anim() {
